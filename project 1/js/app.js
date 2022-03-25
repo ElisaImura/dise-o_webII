6e86a5f9c8ca74825207614afabb02c8bd5f70d8
yuri_imura <imurayuri4503@gmail.com>
2022-03-24 21:08:16 -0300

    Envio de tarea

---
+++ project 1/js/app.js
@@ -0,0 +1,10 @@
+var array=[];
+array=[1,2,3,4,5,6];
+
+var suma=0;
+for(var i=0; i<array.length; i++){
+  suma=suma+array[i];
+}
+
+console.log(array);
+console.log("La suma de todos los elementos es: ", suma);
